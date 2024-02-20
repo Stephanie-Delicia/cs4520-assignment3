ea40a1ba3476fb5f8d0ae6fa2c8c6c31725b5310
Stephanie M <martinez.s@northeastern.edu>
2024-02-20 17:18:32 -0500

    Added navigation from home fragment

--- app/src/main/java/com/cs4520/assignment3/HomeFragment.kt
+++ app/src/main/java/com/cs4520/assignment3/HomeFragment.kt
@@ -5,20 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -27,8 +17,20 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val navHostFragmentController = requireActivity().supportFragmentManager
+            .findFragmentById(R.id.navFragment)?.findNavController()
+        var mvpBtn = view.findViewById<Button>(R.id.mvp_button)
+        var mvvmBtn = view.findViewById<Button>(R.id.mvvm_button)
+        mvpBtn.setOnClickListener{
+            navHostFragmentController?.navigate(R.id.action_homeFragment_to_MVPFragment)
+        }
+
+        mvvmBtn.setOnClickListener{
+            navHostFragmentController?.navigate(R.id.action_homeFragment_to_MVVMFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
 }
