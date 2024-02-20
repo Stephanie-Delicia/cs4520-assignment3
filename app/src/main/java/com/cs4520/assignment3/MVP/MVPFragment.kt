31b633de9dafe78aa14da2a896c8a4cf7b6341d2
Stephanie M <martinez.s@northeastern.edu>
2024-02-20 17:46:33 -0500

    Moved MVP/MVVM fragments to their own folders.

---
+++ app/src/main/java/com/cs4520/assignment3/MVP/MVPFragment.kt
@@ -0,0 +1,22 @@
+package com.cs4520.assignment3.MVP
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cs4520.assignment3.R
+
+class MVPFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mvp, container, false)
+    }
+}
